dc60b2b7de9f5f167904a51919e66918979b69d8
Ash Bridges <ashbridgescodes@gmail.com>
2023-06-18 17:56:10 +0100

    added backend to like/unlike and check like status

---
+++ util/spotify/fetchLikeStatus.ts
@@ -0,0 +1,26 @@
+export const fetchLikeStatus = async (id: string) => {
+  try {
+    const res = await fetch(`/api/spotifyLikeStatus/${id}`);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const likeSpotifySong = async (id: string) => {
+  try {
+    await fetch(`/api/spotifyLike/${id}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const unlikeSpotifySong = async (id: string) => {
+    try {
+      await fetch(`/api/spotifyUnlike/${id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  
