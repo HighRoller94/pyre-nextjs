e943b39252d77693c267fade704ff30a14d20e17
Ash Bridges <ashbridgescodes@gmail.com>
2023-08-16 11:36:30 +0100

    fixed media skels key

--- components/Skels/MediaContainerSkel.tsx
+++ components/Skels/MediaContainerSkel.tsx
@@ -4,7 +4,7 @@ const MediaContainerSkel = () => {
   return (
     <div className="px-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 my-4">
       {[...Array(8).keys()].map((i) => (
-        <MediaSkel />
+        <MediaSkel key={i} />
       ))}
     </div>
   );
